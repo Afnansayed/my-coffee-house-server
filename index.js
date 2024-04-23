09873a8b930e034650f55e71aad1e5d74cf91cab
Afnansayed <afnansayed1973@gmail.com>
2024-04-24 05:35:53 +0600

    update/put server code

--- index.js
+++ index.js
@@ -48,6 +48,27 @@ async function run() {
         res.send(result);
     })
 
+    //put/update
+    app.put('/coffee/:id',async (req,res)=>{
+         const coffee = req.body;
+         const id = req.params.id;
+         const filter = {_id: new ObjectId(id)};
+         const options = {upsert: true};
+         const updatedCoffee = {
+            $set:{
+                name: coffee.name,
+                quantity:coffee.quantity,
+                supplier:coffee.supplier,
+                taste:coffee.taste,
+                category:coffee.category,
+                details:coffee.details,
+                photo:coffee.photo
+            }
+         }
+         const result = await coffeeCollection.updateOne(filter,updatedCoffee,options);
+         res.send(result)
+    })
+
     //delete
     app.delete('/coffee/:id',async(req,res) => {
         const id = req.params.id;
